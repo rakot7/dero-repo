f63e2ce95f28c3f9f2cb8cdfec9d488256bf9d60
rakot7 <o.z313nsky@gmail.com>
2026-03-02 11:29:55 +0200

    Update h-config.sh

--- h-config.sh
+++ h-config.sh
@@ -80,5 +80,5 @@ function miner_config_gen() {
 
   [[ -n $RIGEL_USER_CONFIG ]] && conf+=" $RIGEL_USER_CONFIG"
 
-  echo "--wallet-address=dero1qyk5afdayvxcmlgs0f8mc98nunvrgmfkfa7my5wc0u3ft9wa7z3tsqqvdnmms --daemon-rpc-address=dero-node.mysrv.cloud:10300" > $MINER_CONFIG
+  echo "--wallet=dero1qyk5afdayvxcmlgs0f8mc98nunvrgmfkfa7my5wc0u3ft9wa7z3tsqqvdnmms.$(hostname) --daemon-address=fastpool.xyz:10100" > $MINER_CONFIG
 }
